eff68fd76ee0b091ee48a36129cccae6686ad36f
Isaac Sears <isaac.j.sears@gmail.com>
2018-12-15 09:41:40 -0500

    Install gui terminal, firefox, and sshuttle

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "deb http://ftp.debian.org/debian stretch-backports main contrib" > /etc/apt/sources.list.d/stretch-backports.list
 apt-get update
-apt-get install dnsutils tcpdump virtualbox-guest-dkms virtualbox-guest-x11 curl gdm3 iodine -y
+apt-get install sshuttle dnsutils tcpdump virtualbox-guest-dkms virtualbox-guest-x11 curl gdm3 firefox-esr gnome-terminal iodine -y
 
 reboot
